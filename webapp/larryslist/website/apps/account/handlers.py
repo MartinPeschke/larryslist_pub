2c78199f8d78b63359f66d4928e089191f8a9b47
Martin Peschke <martin@per-4.com>
2013-03-26 12:10:32 +0100

    reports tool config updates

--- webapp/larryslist/website/apps/account/handlers.py
+++ webapp/larryslist/website/apps/account/handlers.py
@@ -1,6 +1,12 @@
 from larryslist.lib.formlib.handlers import FormHandler
 from .forms import PasswordResetForm
+from larryslist.website.apps.models import RefreshUserProfileProc
 
 
 class ProfileHandler(FormHandler):
     form = PasswordResetForm
+
+    def GET(self):
+        request = self.request
+        RefreshUserProfileProc(request, {'token':request.root.user.token})
+        return super(ProfileHandler, self).GET()
